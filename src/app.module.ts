521829440cccb5d5591d717194ddd180da5f6ea7
FreedomToTibet <98056258+FreedomToTibet@users.noreply.github.com>
2025-10-20 01:18:20 -0700

    Integrate Mongoose and add Product schema
    Configured Mongoose in the app and product modules to connect to MongoDB and use the Product schema. Added the Product schema definition for MongoDB integration.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose'; // 1.1 Import the mongoose module
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [ProductModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/store'), // 1.2 Configure the mongoose module
+    ProductModule, // 2.2 Add the product module
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
